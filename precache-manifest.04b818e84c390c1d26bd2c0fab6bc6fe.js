f8d9864559b9f38231c360dabe95faf19a2cab42
sdavran <sumeyra.ozgur91@gmail.com>
2019-09-09 04:38:53 +0300

    Updates

---
+++ precache-manifest.04b818e84c390c1d26bd2c0fab6bc6fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89592592347639661963bd30f3f5d9c",
+    "url": "/dollar-react/index.html"
+  },
+  {
+    "revision": "0943c652f1ef07a454d3",
+    "url": "/dollar-react/static/css/main.05f46209.chunk.css"
+  },
+  {
+    "revision": "0b4bd5a1bc134559ba04",
+    "url": "/dollar-react/static/js/2.0847caae.chunk.js"
+  },
+  {
+    "revision": "0943c652f1ef07a454d3",
+    "url": "/dollar-react/static/js/main.d0c44fb3.chunk.js"
+  },
+  {
+    "revision": "61d73e07eecc3f064003",
+    "url": "/dollar-react/static/js/runtime~main.12986b25.js"
+  }
+]);
